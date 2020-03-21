7c1c8bcf7038ac73670b737f8e5393413726845f
rejaulmondal <rejaulmondal786@gmail.com>
2020-03-21 11:53:18 +0530

    AngryBirdStage8.5

--- Bird.js
+++ Bird.js
@@ -12,6 +12,8 @@ class Bird extends BaseClass {
 
     super.display();
 
+    console.log(bird.body.speed);
+
     if(this.body.velocity.x > 10 && this.body.position.x > 200){
       var position = [this.body.position.x, this.body.position.y];
       this.trajectory.push(position);
--- sketch.js
+++ sketch.js
@@ -77,15 +77,16 @@ function draw(){
     bird.display();
     platform.display();
     //log6.display();
-    slingshot.display();    
+    slingshot.display();  
+    
+    
 }
 
 function mouseDragged(){
     //if (gameState!=="launched"){
         Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
-    //}
+   //}
 }
-
 
 function mouseReleased(){
     slingshot.fly();
@@ -93,8 +94,10 @@ function mouseReleased(){
 }
 
 function keyPressed(){
-    if(keyCode === 32){
-       slingshot.attach(bird.body);
+    if(keyCode === 32 && bird.body.speed < 1){
+        bird.trajectory = [];
+        Matter.Body.setPosition(bird.body, {x: 200 , y: 50});
+        slingshot.attach(bird.body);
     }
 }
 
@@ -105,7 +108,7 @@ async function getBackgroundImg(){
     var datetime = responseJSON.datetime;
     var hour = datetime.slice(11,13);
     
-    if(hour>=0600 && hour<=1900){
+    if(hour >= 06 && hour <= 18){
         bg = "sprites/bg1.png";
     }
     else{
